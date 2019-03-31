789db39ef8e4fff876d4933bd46c236115a72901
Henrik Aarnio <henrik@blok.ai>
2019-03-31 10:43:12 +0300

    face swap function, histogram matching

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,19 @@ FROM python:3.6
 # Install dependencies
 RUN apt-get update && apt-get install -y cmake
 
-COPY requirements.txt /app/requirements.txt
-
-WORKDIR /app
 RUN pip install dlib==19.17.0
+
+# Install requirements
+COPY requirements.txt /app/requirements.txt
+WORKDIR /app
 RUN pip install -r requirements.txt
+
+# Install newer scikit-image
+RUN apt-get install -y python3-matplotlib python3-numpy python3-pil python3-scipy python3-tk
+RUN apt-get install -y build-essential cython3
+RUN git clone https://github.com/scikit-image/scikit-image.git
+RUN cd scikit-image && pip3 install -e .
+
 
 COPY app /app/app
 
--- app/face_finder.py
+++ app/face_finder.py
@@ -16,46 +16,16 @@
 
 import dlib
 import numpy as np
-
-
-TEMPLATE = np.float32([
-    (0.0792396913815, 0.339223741112), (0.0829219487236, 0.456955367943),
-    (0.0967927109165, 0.575648016728), (0.122141515615, 0.691921601066),
-    (0.168687863544, 0.800341263616), (0.239789390707, 0.895732504778),
-    (0.325662452515, 0.977068762493), (0.422318282013, 1.04329000149),
-    (0.531777802068, 1.06080371126), (0.641296298053, 1.03981924107),
-    (0.738105872266, 0.972268833998), (0.824444363295, 0.889624082279),
-    (0.894792677532, 0.792494155836), (0.939395486253, 0.681546643421),
-    (0.96111933829, 0.562238253072), (0.970579841181, 0.441758925744),
-    (0.971193274221, 0.322118743967), (0.163846223133, 0.249151738053),
-    (0.21780354657, 0.204255863861), (0.291299351124, 0.192367318323),
-    (0.367460241458, 0.203582210627), (0.4392945113, 0.233135599851),
-    (0.586445962425, 0.228141644834), (0.660152671635, 0.195923841854),
-    (0.737466449096, 0.182360984545), (0.813236546239, 0.192828009114),
-    (0.8707571886, 0.235293377042), (0.51534533827, 0.31863546193),
-    (0.516221448289, 0.396200446263), (0.517118861835, 0.473797687758),
-    (0.51816430343, 0.553157797772), (0.433701156035, 0.604054457668),
-    (0.475501237769, 0.62076344024), (0.520712933176, 0.634268222208),
-    (0.565874114041, 0.618796581487), (0.607054002672, 0.60157671656),
-    (0.252418718401, 0.331052263829), (0.298663015648, 0.302646354002),
-    (0.355749724218, 0.303020650651), (0.403718978315, 0.33867711083),
-    (0.352507175597, 0.349987615384), (0.296791759886, 0.350478978225),
-    (0.631326076346, 0.334136672344), (0.679073381078, 0.29645404267),
-    (0.73597236153, 0.294721285802), (0.782865376271, 0.321305281656),
-    (0.740312274764, 0.341849376713), (0.68499850091, 0.343734332172),
-    (0.353167761422, 0.746189164237), (0.414587777921, 0.719053835073),
-    (0.477677654595, 0.706835892494), (0.522732900812, 0.717092275768),
-    (0.569832064287, 0.705414478982), (0.635195811927, 0.71565572516),
-    (0.69951672331, 0.739419187253), (0.639447159575, 0.805236879972),
-    (0.576410514055, 0.835436670169), (0.525398405766, 0.841706377792),
-    (0.47641545769, 0.837505914975), (0.41379548902, 0.810045601727),
-    (0.380084785646, 0.749979603086), (0.477955996282, 0.74513234612),
-    (0.523389793327, 0.748924302636), (0.571057789237, 0.74332894691),
-    (0.672409137852, 0.744177032192), (0.572539621444, 0.776609286626),
-    (0.5240106503, 0.783370783245), (0.477561227414, 0.778476346951)])
-
-TPL_MIN, TPL_MAX = np.min(TEMPLATE, axis=0), np.max(TEMPLATE, axis=0)
-MINMAX_TEMPLATE = (TEMPLATE - TPL_MIN) / (TPL_MAX - TPL_MIN)
+import random
+import nudged
+from skimage import transform
+from skimage import morphology
+from PIL import Image
+from PIL import ImageDraw
+from skimage.filters import gaussian
+from skimage.segmentation import active_contour
+from skimage.color import rgb2gray
+from skimage.transform import match_histograms
 
 
 class AlignDlib:
@@ -141,3 +111,94 @@ class AlignDlib:
 
         points = self.predictor(rgbImg, bb)
         return list(map(lambda p: (p.x, p.y), points.parts()))
+
+
+finder = AlignDlib("shape_predictor_5_face_landmarks.dat")
+
+
+def coords_from_pil_image(image):
+    np_img = np.array(image)
+    return finder.getAllFaceBoundingBoxes(np_img)
+
+
+def landmarks_from_pil_image(image):
+    coords = coords_from_pil_image(image)
+    np_img = np.array(image)
+    return [
+        finder.findLandmarks(np_img, coord)
+        for coord in coords
+    ]
+
+
+def generate_histogram(image, mask):
+    mask_pixels = mask.getdata()
+    image_pixels = [
+        imgage_pixel
+        for (imgage_pixel, mask_pixel)
+        in zip(image.getdata(), mask_pixels)
+        if mask_pixel > 0
+    ]
+    histogram_bg = Image.new('RGB', image.size)
+    histogram_bg_pixels = histogram_bg.load()
+    for i in range(histogram_bg.size[0]):
+        for j in range(histogram_bg.size[1]):
+            histogram_bg_pixels[i, j] = image_pixels[random.randint(0, len(image_pixels)-1)]
+    return Image.composite(image, histogram_bg, mask)
+
+
+def swap_faces(img1, img2):
+    # get landmark points from the images
+    landmarks1 = landmarks_from_pil_image(img1)
+    landmarks2 = landmarks_from_pil_image(img2)
+    img1_index = random.randint(0, len(landmarks1) - 1)
+    img2_index = random.randint(0, len(landmarks2) - 1)
+    landmarks1 = landmarks1[img1_index]
+    landmarks2 = landmarks2[random.randint(0, len(landmarks2) - 1)]
+    bb = coords_from_pil_image(img1)[img1_index]
+    bb = coords_from_pil_image(img2)[img2_index]
+    # calculate transformation matrix to go from one set of points to the other
+    trans_matrix1 = nudged.estimate(landmarks2, landmarks1).get_matrix()
+
+    trans1 = transform.ProjectiveTransform(matrix=np.array(trans_matrix1))
+    # transform the images to be on top of each other
+    img1_transformed = transform.warp(
+        np.array(img1),
+        trans1,
+        output_shape=np.array(img2).shape[:2]
+    ).dot(255).astype('uint8')
+
+    s = np.linspace(0, 2*np.pi, 400)
+    height = (bb.bottom()-bb.top())
+    rad_y = height/2*1.2
+    x = (bb.left()+bb.right())/2 + (bb.right()-bb.left())/2*np.cos(s)
+    y = (bb.top()+bb.bottom())/2 + rad_y*np.sin(s) - height*0.15
+    init = np.array([x, y]).T
+    #init = init.dot(trans_matrix1)
+    shape = active_contour(
+        gaussian(rgb2gray(img1_transformed), 3),
+        init, alpha=0.08, beta=1, gamma=0.001, max_iterations=height*0.1, max_px_move=1
+    )
+    #shape = init
+    output = Image.fromarray(img1_transformed)
+    mask = Image.new('L', output.size)
+    draw = ImageDraw.Draw(mask)
+    draw.polygon([tuple(coord)[:2] for coord in shape], fill="white")
+    #draw = ImageDraw.Draw(img2)
+    #draw.polygon([tuple(coord)[:2] for coord in shape], outline="red")
+    #return img2
+
+    # Match histograms on the selected areas
+    print(morphology.disk(int(height*0.1)))
+    hist_mask = Image.fromarray(morphology.dilation(np.array(mask), morphology.disk(int(height*0.1))))
+    img2_histogram = generate_histogram(img2, hist_mask)
+    img1_histogram = generate_histogram(Image.fromarray(img1_transformed), hist_mask)
+    matched = Image.fromarray(match_histograms(
+        np.asarray(img1_histogram),
+        np.asarray(img2_histogram),
+        multichannel=True
+    ))
+
+    mask = gaussian(np.array(mask), height*0.1)
+    mask = Image.fromarray(mask.dot(255).astype('uint8'))
+    output = Image.composite(matched, img2, mask)
+    return output
